1ae8d7a3554d6979ec091b80be181bb7df5a777c
drdreflow377 <105461373+drdreflow377@users.noreply.github.com>
2024-06-30 09:57:41 -0400

    Add files via upload

---
+++ tests/test_sar_propensity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `sar_propensity` package."""
+
+
+import unittest
+
+from sar_propensity import sar_propensity
+
+
+class TestSar_propensity(unittest.TestCase):
+    """Tests for `sar_propensity` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
